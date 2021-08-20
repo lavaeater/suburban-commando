df1526e7f738c3e12ae02f68a567a771e033ee82
Tommie Nygren <tommie.nygren@gmail.com>
2021-08-20 08:56:10 +0200

    Shooting Now the player can shoot using a raycast

--- src/entities/player/PlayerIsometric.gd
+++ src/entities/player/PlayerIsometric.gd
@@ -8,6 +8,8 @@ var direction = Vector3()
 var spin = .1
 var jump = false
 var look_at_target = Vector3()
+onready var rayBlaster = $blasterA.get_node("RayCast")
+var fire = false
 
 func _ready():
 	pass # Replace with function body.
@@ -19,13 +21,30 @@ func _physics_process(delta):
 	if jump and is_on_floor():
 		velocity.y = jump_speed
 	look_at(look_at_target, Vector3.UP)
+	handle_shots()
+
+func handle_shots():
+	if fire and rayBlaster.is_colliding():
+		var collided_with = rayBlaster.get_collider()
+		collided_with.queue_free()
+		#Do something!
+		var tyypeasfas = ""
 
 func get_input():
 	velocity.x = 0
 	velocity.z = 0
 	direction.x = 0
 	direction.z = 0
+	handle_movement()
+	handle_jumping()
+	handle_shooting()
 	
+func handle_shooting():
+	fire = false
+	if Input.is_action_pressed("fire_primary"):
+		fire = true
+
+func handle_movement():
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= speed 
 	if Input.is_action_pressed("move_back"):
@@ -37,6 +56,8 @@ func get_input():
 	direction = direction.rotated(Vector3.UP, PI / 4)
 	velocity.x = direction.x
 	velocity.z = direction.z
+
+func handle_jumping():
 	jump = false
 	if Input.is_action_just_pressed("jump"):
 		jump = true
