ae9f7faa281e4ba8798c039aa203f0110fa2cae1
Tommie Nygren <tommie.nygren@gmail.com>
2021-09-03 09:20:19 +0200

    Fixed reference bug in debug overlay

--- level/Main.gd
+++ level/Main.gd
@@ -2,11 +2,12 @@ extends Spatial
 
 
 onready var enemy = get_node("Enemy")
-onready var behavior_tree = get_node("Enemy/EnemyBehavior")
 
 onready var floor_width = $Floor.get_node("MeshInstance").mesh.size.x
 onready var floor_depth = $Floor.get_node("MeshInstance").mesh.size.y
 onready var floor_center = $Floor.global_transform.origin
+
+onready var node_ref = weakref(get_node("Enemy"))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +20,9 @@ func _ready():
 	overlay.add_stat_method("Enemy Target Position", self, "get_target_vector")
 	add_child(overlay)
 
+func get_enemy():
+	return node_ref.get_ref()
+
 func get_player_velocity():
 	return $Player.velocity
 
@@ -30,19 +34,24 @@ func get_player_position():
 
 func get_enemy_position():
 	var t = Vector3.ZERO
+	var enemy = get_enemy()
 	if enemy != null:	
 		t = enemy.global_transform.origin
 	return t
 	
 func get_target_vector():
 	var t = Vector3.ZERO
+	var enemy = get_enemy()
 	if enemy != null:
 		t = enemy.target_vector
 	return t
 	
 func get_current_enemy_action():
 	name = "Enemy Action Pending"
-	if enemy != null and behavior_tree != null and behavior_tree.tree != null:
-		name = "Enemy Action: " + behavior_tree.tree.current_task_running
-	return name
+	var enemy = get_enemy()
+	if enemy != null:
+		var behavior_tree = enemy.get_node("Enemy/EnemyBehavior")
+		if behavior_tree != null and behavior_tree.tree != null:
+			name = "Enemy Action: " + behavior_tree.tree.current_task_running
+			return name
 
