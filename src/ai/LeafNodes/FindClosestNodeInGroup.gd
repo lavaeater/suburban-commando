08f9a054e7a3fd525753f7038d7d9f715e184ff4
Tommie Nygren <tommie.nygren@gmail.com>
2021-09-13 06:26:07 +0200

    ENemy moves like madman, but it moves towards player now

--- src/ai/LeafNodes/FindClosestNodeInGroup.gd
+++ src/ai/LeafNodes/FindClosestNodeInGroup.gd
@@ -4,6 +4,7 @@ class_name FindClosestNodeInGroup, "res://addons/behavior_tree/icons/btleaf.svg"
 export var GroupName = "Resource"
 export var DetectionDistance = 100.0
 export var BlackBoardKey = "ClosestResource"
+export var CheckProperty = true
 export var PropertyToCheck = "Energy"
 export var PropertyValue = 80
 
@@ -18,7 +19,8 @@ func _pre_tick(agent: Node, blackboard: Blackboard) -> void:
 	for resource in resources:
 		var property_level = resource.get(PropertyToCheck)
 		var new_dist = agent.global_transform.origin.distance_squared_to(resource.global_transform.origin)
-		if new_dist < distance and property_level > PropertyValue:
+		var check_prop = !CheckProperty or property_level > PropertyValue
+		if new_dist < distance and check_prop:
 			distance = new_dist
 			closest_resource = resource
 	if distance < DetectionDistance:
