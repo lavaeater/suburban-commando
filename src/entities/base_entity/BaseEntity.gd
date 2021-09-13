341bf72cec3249fbffa7a2bdac2ec121dfc48cdd
Tommie Nygren <tommie.nygren@gmail.com>
2021-09-13 06:39:02 +0200

    Health Now on Blackboard

--- src/entities/base_entity/BaseEntity.gd
+++ src/entities/base_entity/BaseEntity.gd
@@ -4,18 +4,23 @@ export var health = 100
 var dead = false
 signal death_occurred
 signal took_damange
+export (NodePath) var Blackboard
+export (String) var HealthProperty
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func take_damage(damage):
-	health -= damage
+	var blackboard = get_node(Blackboard)
+	if blackboard != null:
+		health = blackboard.get_data(HealthProperty)
+		health -= damage
+		blackboard.set_data(HealthProperty, health)
+	else:
+		health -= damage
+
 	emit_signal("took_damange")
 	if health < 0:
 		dead = true
 		emit_signal("death_occurred")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
