d4db6358fc6e9612ae1fd7e6bf4bb4f243a40fa8
Tommie Nygren <tommie.nygren@gmail.com>
2021-09-14 05:10:25 +0200

    Added an enemy spawner

--- src/entities/things/enemy_spawner/EnemySpawner.gd
+++ src/entities/things/enemy_spawner/EnemySpawner.gd
@@ -1,11 +1,13 @@
-extends StaticBody
+extends Spatial
 
+export (String) var Group = "Enemies" 
+export (int) var MinimumSpawned = 10
+export (int) var MaximumSpawned = 100
+export (float) var CoolDown = 1.0
+export (String) var SceneToSpawn = "res://src/entities/enemy-two/Enemy2.tscn"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-export var Energy = 100
+onready var ActualCoolDown = CoolDown
+onready var ToSpawn = load(SceneToSpawn)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,5 +15,15 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	ActualCoolDown -= delta
+	if ActualCoolDown < 0:
+		ActualCoolDown = CoolDown
+		var count = get_tree().get_nodes_in_group(Group).size()
+		if count < MinimumSpawned or count < MaximumSpawned:		 
+			var newly_spawned = ToSpawn.instance()
+			newly_spawned.add_to_group(Group)
+			var parent = get_parent()
+			remove_child(newly_spawned)
+			parent.add_child(newly_spawned)
+		
