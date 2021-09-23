d7b5a1054e7ee6e6215d10e366a253778d420c04
Tommie Nygren <tommie.nygren@gmail.com>
2021-09-23 13:55:18 +0200

    Snowballs bounce a couple of times

--- src/entities/projectile/projectile.gd
+++ src/entities/projectile/projectile.gd
@@ -1,5 +1,6 @@
 extends RigidBody
 export var force = 100
+export var bounce_count = 2
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -16,5 +17,7 @@ func _ready():
 
 
 func _on_Projectile_body_entered(body):
+	bounce_count -= 1
 	# Check if it is an enemy. Regardless, this one is being removed now
-	queue_free()
+	if bounce_count <= 0:
+		queue_free()
